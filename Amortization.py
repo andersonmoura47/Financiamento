104c96630bdfa8fc94cf09f8368df767c62be9e1
Anderson Moura <63905587+andersonmoura47@users.noreply.github.com>
2025-02-13 18:08:29 -0300

    Update Amortization.py

--- Amortization.py
+++ Amortization.py
@@ -13,15 +13,33 @@ Comentários:
         Guithub: Anderson Moura - andersonmoura47
         Instagram: anderson_moura47
 """
+#~~~~~~~~~~~~~~~~~~~~~~~~~~> SELECIONA SISTEMA <~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def seleciona_sistema():
+    q = True
+    while q == True:
+        sist = int(input('\nQual sistema deseja utilizar?\n1 - SAC\n2 - PRICE\n3 - SAA\n'))
+        if sist == 1:
+            a = sac(pv, i, n)
+            q = False
+        elif sist == 2:
+            a = price(pv, i, n)
+            q = False
+        elif sist == 3:
+            a = saa(pv, i, n)
+            q = False
+        else:
+            q = False
+    return a
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~> CONVERSÃO DE TAXA <~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def conversao_de_taxa(i):
     v = True
     while v == True: # validacao da pergunta 1 (converter taxa?)
-        p = int(input('Deseja converter a taxa?\n1 - Sim\n2 - Não'))
+        p = int(input('\nDeseja converter a taxa?\n1 - Sim\n2 - Não\n'))
         if p == 1:
             v2 = True
             while v2 == True: # validacao da pergunta 2 (periodos para convercoes de taxa)
-                p2 = int(input('Escolha a opção de conversão desejada: \n1 - Anual para mensal \n2 - Mensal para Anual'))
+                p2 = int(input('\nEscolha a opção de conversão desejada: \n1 - Anual para mensal \n2 - Mensal para Anual\n'))
                 if p2 == 1:
                     i = (((1 + (i/100)) ** (1/12)) - 1) * 100 # conversao de a.a para a.m
                     v2 = False # sair da p2
@@ -110,29 +128,20 @@ def saa(pv, i, n):
         parc.append(float('%.2f' %pmt))
     return (parc)
 
+
 #==========================================CHAMANDO AS FUNÇÕES=======================================
-pv = 100000
-i = 0.64 # taxa mensal (7.95% a.a)
-n = 12
+pv = int(input('Digite o valor: '))
+i = int(input('Digite a taxa: ')) # taxa mensal (7.95% a.a)
+n = int(input('Digite o numero de periodos: '))
 
-a = sac(pv, i, n)
-b = price(pv, i, n)
-c = saa(pv, i, n)
+a = seleciona_sistema()
 
-#==========================================Comparações gráficas=======================================
-import matplotlib.pyplot as plt
-local_eixo_x = 1
-for valores in (a,b,c): # em cada loop é utlilizado uma das listas com os valores das parcelas
-    lista = []
-    for i in range(len(valores)):
-        lista.append(i + local_eixo_x) # cria uma lista com numeros para o eixo x
-    local_eixo_x += len(valores) + 1
-    plt.bar(lista, valores, label = 'Valor médio Parcelas: R$%.2f | Valor Total: R$%.2f' %(sum(valores) / len(valores), sum(valores)))
-plt.title('Evolução da parcelas em cada financiamento')
-plt.xlabel('Tempo')
-plt.ylabel('Valores das parcelas em (R$)')
-plt.legend()
-plt.show()
+
+print('\nValor: R$',pv, '\nTaxa: ',i,'%', '\nParcelas: ', n)
+print('\nValor das parcelas:')
+for j in range(len(a)):
+    print(j + 1,' - R$', a[j])
+print('\nValor total pago: R$', sum(a),'\n')
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> Anotações - formas de calculo: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
